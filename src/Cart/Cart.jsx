ed65a73ff4c3bc7c3c1ef979f9e7e7ffc9535f44
Sari ab <bnsari789@gmail.com>
2025-01-12 09:17:34 +0200

    it just works

--- src/Cart/Cart.jsx
+++ src/Cart/Cart.jsx
@@ -1,11 +1,26 @@
 import styles from "./Cart.module.css";
 
 export default function Cart({ data }) {
+    let total = 0;
+
     return (
         <div className={styles.mainCont}>
             <div className={styles.borderWrapper}>
                 <div className={styles.itemCont}>
-                    placeholder
+                    {data.filter(item => item.count > 0).map((item) => {
+                        const text = item.item.length > 10 ? item.item.substring(0, 7) + "..." : item.item;
+                        total = total + item.price * item.count;
+                        return (
+                            <div key={item.item} className={styles.item}>
+                                <img className={styles.image} src={item.image} alt={item.item} />
+                                <div className={styles.textCont}>
+                                    <p>{text}</p>
+                                    <p>{item.count + " piece(s): " + item.count * item.price + "$"}</p>
+                                </div>
+                            </div>
+                        )
+                    })}
+                    <p>{"Total: " + total + "$"}</p>
                 </div>
             </div>
         </div>
